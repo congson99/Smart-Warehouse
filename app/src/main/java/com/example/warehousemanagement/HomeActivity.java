2aaabbc6282517514d9779a26a4cac9a627d7597
Công Sơn <congson99vn@gmail.com>
2020-06-28 14:21:00 +0700

    Update Home UI
    Add Home UI

--- app/src/main/java/com/example/warehousemanagement/HomeActivity.java
+++ app/src/main/java/com/example/warehousemanagement/HomeActivity.java
@@ -1,18 +1,38 @@
 package com.example.warehousemanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button bt_en;
-    Button bt_we;
-    Button bt_av;
+    DatabaseReference databaseReference;
+
+    TextView name;
+    ImageView avatar;
+    CardView bt_environment;
+    CardView bt_weather;
+    CardView bt_history;
+    CardView bt_products;
+    CardView bt_aboutus;
+    CardView bt_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +44,40 @@ public class HomeActivity extends AppCompatActivity {
         final String id = intentf.getStringExtra("id");
 
         //Anh xa
-        bt_en = (Button) findViewById(R.id.home_bt_enviroment);
-        bt_we = (Button) findViewById(R.id.home_bt_weather);
-        bt_av = (Button) findViewById(R.id.home_bt_avatar);
+        name = (TextView) findViewById(R.id.home_name);
+        avatar = (ImageView) findViewById(R.id.home_circle_img_avt);
+        bt_environment = (CardView) findViewById(R.id.home_acti_environment_card);
+        bt_weather = (CardView) findViewById(R.id.home_acti_weather_card);
+        bt_account = (CardView) findViewById(R.id.home_acti_account_card);
 
-        //Enviroment_button
-        bt_en.setOnClickListener(new View.OnClickListener() {
+        //link to account info
+        databaseReference = FirebaseDatabase.getInstance().getReference("Account");
+
+        //Set value
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Set value
+                avatar.setImageResource(R.drawable.farmericon);
+                if(!dataSnapshot.child(id).child("Avatar").getValue().toString().equals("None")){
+                    byte[] mangGet = Base64.decode(dataSnapshot.child(id).child("Avatar").getValue().toString(), Base64.DEFAULT);
+                    Bitmap bm = BitmapFactory.decodeByteArray(mangGet, 0, mangGet.length);
+                    avatar.setImageBitmap(bm);
+                }
+                name.setText(dataSnapshot.child(id).child("Name").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //Environment_button
+        bt_environment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent to Enviroment
+                //Intent to Environment
                 Intent intent = new Intent(HomeActivity.this, EnvironmentActivity.class);
                 intent.putExtra("id", id);
                 startActivity(intent);
@@ -40,7 +85,7 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         //Weather_button
-        bt_we.setOnClickListener(new View.OnClickListener() {
+        bt_weather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Intent to Weather
@@ -50,11 +95,11 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        //Avatar_button
-        bt_av.setOnClickListener(new View.OnClickListener() {
+        //Account_button
+        bt_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent to Weather
+                //Intent to Avatar
                 Intent intent = new Intent(HomeActivity.this, InfoActivity.class);
                 intent.putExtra("id", id);
                 startActivity(intent);
